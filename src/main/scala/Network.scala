ec8a5e3b8783612a97a886bf0f15bde1bea57c7c
tanitta <ttata.trit@gmail.com>
2013-11-10 03:28:16 +0900

    通信機能の実装開始．Serverの初期化はできた．

---
+++ src/main/scala/Network.scala
@@ -0,0 +1,9 @@
+package trit.alight
+import processing.core._
+import processing.net._
+class Network(var ps:PApplet){
+	var server = new Server(ps, 20000)
+	def setup = {}
+	def update = {}
+	def draw = {}
+}
