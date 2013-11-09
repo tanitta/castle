4810e9272d95379e610afaa814ff6191af117d3d
tanitta <ttata.trit@gmail.com>
2013-11-10 03:51:23 +0900

    Clientも書き始める

--- src/main/scala/Network.scala
+++ src/main/scala/Network.scala
@@ -4,6 +4,11 @@ import processing.net._
 class Network(var ps:PApplet){
 	var server = new Server(ps, 20000)
 	def setup = {}
-	def update = {}
+	def update = {
+		var c:Client = server.available()
+		if(c != null){
+			var s:String = c.readString()
+		}
+	}
 	def draw = {}
 }
